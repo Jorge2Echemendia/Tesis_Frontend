4901324b1365622208e67ccde8ab16c6a5753d7a
Jorge2Echemendia <jorge2echemendia@gmail.com>
2025-01-11 17:37:10 -0500

    modificdo

--- lib/src/config/config.dart
+++ lib/src/config/config.dart
@@ -1,15 +1,17 @@
-
-
-  class Config {
-     static const String firebaseType = "service_account";
-     static const String projectId = "tesis-f0a5e";
-     static const String privateKeyId = "d357cf49e5f65ba1a1038ba33389685b35d488a9";
-     static const String privateKey = "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCZ/L1g9Gt6BeoC\n+VtkhRPUG4gNDf5K/kCLLNWoXTyblo2flBdfqMON+u5Le+PP6qDRqxijpPLO9TRB\nj5UyufGPkxS0Lxi3mCn5pmyiTY5prqXNXUpwqvn7B4vsdxIDWE8aPywqMQJC+sCm\nICTJM02ZlrbLcleQcgepL+r+2Yd30U8fImT2GlkckKhqDCeM36k2x9bvlJbgR5ta\ndduE0sKeejxShiQ49YBnlR/tTz+GA6k2JuY2vRGPIgW68qd17vu0YMcqk8FY9iBu\nxtFTmt5RGG01GZITIBmgXkRTM98DsHJ188lyP5pCtQjLMYMJWqYpRZYk+xk8VACr\nWupOh4K5AgMBAAECggEABYssKLNH8sHHf9GpFQR29ygCFQE71kiSu4fyza/EnK/V\n+6id0k1ts9z0qv5D8HJolX/IwGxx6to4RkN0ajn4PLRddpKq+8IUQcb4anvrDHoz\ngSISid2X6A4InyvWt8zcNSxQy1iOFan4GUr3NBCfIrBCtq6QQTSfurstRT1A06ME\nvsaS1cAHvB47XNMDpHZ4m5DBrNIKJGsySCLOgNiWaeIvSHV2PzOgQqcZRQ45KMVR\n+bESndEOEzxUXJozozc1qFuKEoqYlIJeihrXWdsQZTImUBA+xDJjttA4gfMfUUf4\nvnpncI04TN0J/GvUapH8MBPG/2uWrOydjgo7usbG8QKBgQDLYGIi2mSIr23JTQOW\no3S5brUJbDUBHO2ISt3hqN6SWHq2scPjIBmgC/wQQjfEpk7wwe7DoYWm5wZ1QmCZ\nptDTwAzFNUq7ejAZoavZskPpq/q9QerGfWMIaK4EH+od03bgqfwLta6ZYf5c6ctZ\noM6aZSJ4SfMIrRup8L/LtSRPqQKBgQDB1NLvmNe8lMw71kMRB5G+/eRrLHdPgv/w\n0bXFrf4reaz8tVWUV5Lz5hqmUm6EWt98HH034AZDR3SDTwws7myMxhkSIUPbGEyi\nGW/SWf6Q5vj8MOo2Zi8BYZPseLdT2smvseI9QomBcfZybqnKXs5XMkIdAb6HnQJV\nYgW8R9HEkQKBgQCw8XOQdUhAr7iqCipUFnSInOw/fXiENmHnUZPIMH8s/nQNnY4b\nfqMs3yB4GNSicDuANoqAhHhe5ON2g8C+DifkA1RZA+u4Py+4LTPyHKn/lNR7cu1L\nsm3GNCUqi4XKW2AzSVFz8qcs5dhDiFmW6lS2ecVKuLQNWWcXLNIB7uDfuQKBgCmo\nWNE/sxYHkMFHclCFpPA8Vswl2VqQV/tkxsE+fMLROrlxK3xfOekLSiz2yGPPt/pP\nkSXiEtaA0+yon4BEmbXDmX3JAnFjDo1EFEFMMCbqTRa+WIfqfC26z/ThmNz/x5ro\nCyyJUU4ttP6xA/LHX2dkGMttJYq11tuM3pixgsbRAoGAbuL5vK+crODanQxPMzNO\nmYEN6v9TJwMR+fL5iQ+fokCFAHJlUhYbvrMl8HfUgPi+A+c9ZulXx32dsmUEpJdM\n6zRcF2KLXOzmkpyfkUmExhfiKF1QoXj9wqDS6+IskjzTgNrOWldPoH1ilswIQHy8\niRBMcVYbTeHTfGw3Nb4DdX8=\n-----END PRIVATE KEY-----\n";
-     static const String clientEmail = "aplicacion-tesis@tesis-f0a5e.iam.gserviceaccount.com";
-     static const String clientId = "101653246232881824194";
-     static const String authUri = "https://accounts.google.com/o/oauth2/auth";
-     static const String tokenUri = "https://oauth2.googleapis.com/token";
-     static const String authProviderCertUrl = "https://www.googleapis.com/oauth2/v1/certs";
-     static const String clientCertUrl = "https://www.googleapis.com/robot/v1/metadata/x509/aplicacion-tesis%40tesis-f0a5e.iam.gserviceaccount.com";
-     static const String universeDomain = "googleapis.com";
-   }
+class Config {
+  static const String firebaseType = "service_account";
+  static const String projectId = "tesis-f0a5e";
+  static const String privateKeyId = "2df38fb7c1e50eb8bff6246d576fe1b0635597e1";
+  static const String privateKey =
+      "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC/JLXwX86UMBEo\npegR61NOhcFkpfV1yr0zGQ48dt/VEf0DuPfapu7LkvnxIomMjXMzzqnBlKLwSeqU\njOn/zCmeBM3w7oVie/eRS0QHsKMwiKbsWOpMnq2qwaVPK3jdUgiowRyge2p2Vwv1\nfFVWv+gTUmaYdqO9Dt1kwpYAzU+iAm5kiL4Kqv6OAO9tfhh9p2nF3+VJIbSbIxGH\nufUYBwSZNjA1MXMmU3AWSr1rhIWetYBzfKeKTyWk0ckWbUYmxqEL7OJQJ/Jdf9/A\nkMlraNcMXWJft8JR/5H2BfSZbMCLhYVUSZliHUAl5budrluRUqf17iXLSOBqYyg6\neY2npcAlAgMBAAECggEADy6F75GsgW36mZU+hC3XcA/aTHmMsCO/jfoaJ130aU1Z\ny0c7EyLZ1KbX3fngSEmeh37ip318HkRrXWEpNNKtJjU7e5IhBgbSow0CBO+tW9gp\nafGl/8VFW0qw1pjyq0wuDo6dYiHAqFBBouiSiVFfctQYp1jE/enbASXIxEeKYP5X\nxYF0R4E6V55bdkTduBKBc6cIyrM5FsLvwhqSaqey9SatZNGkbzVNatsCk/rtgDB7\nplgJ0QnQPDcCi2txaX/S4yqsC3G/fHZ/MKp8vDaEw9rqei7vkaVaQ9jD1KqnDIkn\n/dVcN6tJvMPlYid8E8Ccnf4XwhiYQWNdnYeqvFGAAQKBgQD14zJWEIpc9eigGXEE\nU3LPc3BuBinGT9w8z6EkQKrqYbmXXczZsiR/LMkufvyWB+c6vlonwPsY9od6FKKc\np3DqO42S6sxAeopReZ3wLScP8YU3nfhL8tB1ykN7EEsbnPxztoKOrxC1/Rel27nl\nz+nm3sWL2zOlGnDUAJrCVUFlYQKBgQDHASVmfL3q3SzhMvxbqOIJO3XvkdpeELfS\nX3UvAqpiPZH65iD/LriMtSg97KdWph428hFxLRWPiCnYVIN5QRwv2OLCPh9aoVp9\nqZ7HmimZN7ZpuBrJOnujll1gOCgmahD6MMsBqklSrm36tLtd280x8ixAv4p+Oy2o\nHc6mZ4+NRQKBgQCs67d1kTJNB98t4FZ7rVf7jnqN1aXBQl5nIbl4bkYEiO9lBOFQ\nY5iJG2zsVv+5Xxt9U2rBtFSiDJXgA+0L8DFXeMQqgxdbbRIjFD0/L/Hdo4OFRoHc\nn4l8nN7yB9ZaN//OPJb6UyGUasYbrTfB54LeCuKwZVqfjMW2QiqT6jMQYQKBgEgs\n0Ty3d0XsS6+thztTKA/DIgXudy1wkMra1ixjohoZTI2b01HZxxM5TMwQvaw+eWzi\nJJNQj6ZL8ylDlZ9vjuKHnc5nK4X6SVCUKUMpVXb9bsou/IAJFviAu4L/OwibpWao\nNah38znqi3bG/8GNy4EW3RfqoDciDXAQx3cwzi6lAoGAdiCITq7wS6ZbWMPgDxSU\ntppoXm/bvPxa+niAgawTwkQQxM7u4L8URXFqiHktVgpSSQXHnOCse0kABHi0mfHw\n2/8wd3vLN1T/hLQvR95aALyTq8d5ONU55EqqI0sPDOVkK4JjbNdmNC6ROrLfuhsK\n3cIA9QXBq8ukcOr/d2O9esk=\n-----END PRIVATE KEY-----\n";
+  static const String clientEmail =
+      "tesis-f0a5e@appspot.gserviceaccount.com";
+  static const String clientId = "110071931147950665237";
+  static const String authUri = "https://accounts.google.com/o/oauth2/auth";
+  static const String tokenUri = "https://oauth2.googleapis.com/token";
+  static const String authProviderCertUrl =
+      "https://www.googleapis.com/oauth2/v1/certs";
+  static const String clientCertUrl =
+      "https://www.googleapis.com/robot/v1/metadata/x509/tesis-f0a5e%40appspot.gserviceaccount.com";
+  static const String universeDomain = "googleapis.com";
+}
